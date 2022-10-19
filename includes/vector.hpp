6bc8d06f1805792503aa6b0bc5e12115ba3ab933
Kimhannah <skqkdldhf98@naver.com>
2022-10-19 20:25:53 +0900

    chore: Add PR template

---
+++ includes/vector.hpp
@@ -0,0 +1,16 @@
+//
+// Created by 김한나 on 2022/10/19.
+//
+
+#ifndef FT_CONTAINERS_VECTOR_HPP
+#define FT_CONTAINERS_VECTOR_HPP
+
+namespace ft {
+
+    class vector {
+
+    };
+
+} // ft
+
+#endif //FT_CONTAINERS_VECTOR_HPP
---
+++ srcs/vector.cpp
@@ -0,0 +1,8 @@
+//
+// Created by 김한나 on 2022/10/19.
+//
+
+#include "../includes/vector.hpp"
+
+namespace ft {
+} // ft
