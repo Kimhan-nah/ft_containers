c7c42b39c9675e7d3ed233d6162cdb0eb6aa7d9c
Kimhannah <skqkdldhf98@naver.com>
2022-12-28 15:08:57 +0900

    Add std::vector tmp file for comparing 42::vector

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,13 +1,17 @@
-#include <iostream>
 #include <array>
+#include <iostream>
 
 int main() {
-
   int a[10];
   std::array<int, 3> myArray;
-  std:
-  array();
-//  a.
-  std::cout << "Hello, World!" << std::endl;
+std:
+  // array();
+  //  a.
+  hello world
+
+          std::cout
+      << "Hello, World!" << std::endl;
+  std::cout << "djsaklfjdslkjflksjadlfjsadlkfj;adfjlkdsjfkl;ajslkfjdlsajf;"
+               "skjlfjs;dkfjlsdjflkasdjlkfjsdlf";
   return 0;
 }
