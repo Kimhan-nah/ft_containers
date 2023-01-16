3be2aa19706a1a0c205ce5827ee3d07830b735ad
hannkim <hannkim@student.42seoul.kr>
2023-01-16 17:19:50 +0900

    Add iterator_traits, reverse_iterator

--- includes/iterator.hpp
+++ includes/iterator.hpp
@@ -1,5 +1,5 @@
 /**
- * @file iterator.hpp
+ * @file iterator_traits.hpp
  * @author hannkim (hannkim@student.42seoul.kr)
  * @brief
  * @version 0.1
@@ -12,18 +12,107 @@
 #ifndef ITERATOR_HPP
 #define ITERATOR_HPP
 
+// std::ptrdiff_t
+#include <cstddef>
+#include <deque>
+#include <iterator>
+#include <map>
+#include <vector>
+
 namespace ft {
-template <typename Iterator>
-class reverse_iterator
-    : public iterator<typename iterator_traits<Iterator>::iterator_category,
-                      typename iterator_traits<Iterator>::value_type,
-                      typename iterator_traits<Iterator>::pointer,
-                      typename iterator_traits<Iterator>::reference> {
- public:
-  // Member types
-  typedef Iterator iterator_type;
+// iterator_tags Iterator Tags
+// struct input_iterator_tag {};
+// struct output_iterator_tag {};
+// struct forwward_iterator_tag : public input_iterator_tag {};
+// struct bidirectional_iterator_tag : public forwward_iterator_tag {};
+// struct random_access_iterator_tag : public bidirectional_iterator_tag {};
+
+// iterator class (common iterator class)
+template <typename Category, typename T, typename Distance = std::ptrdiff_t,
+          typename Pointer = T *, typename Reference = T &>
+struct iterator {
+  typedef Category iterator_category;
+  typedef T value_type;
+  typedef Distance difference_type;
+  typedef Pointer pointer;
+  typedef Reference reference;
 };
 
-}  // namespace ft
+// SECTION 1. iterator_traits
+template <typename Iter>
+struct iterator_traits {
+ public:
+  // Member types
+  typedef typename Iter::difference_type difference_type;
+  typedef typename Iter::value_type value_type;
+  typedef typename Iter::pointer pointer;
+  typedef typename Iter::reference reference;
+  typedef typename Iter::iterator_category iterator_category;
+};
 
+// template specialization <T *>
+template <typename Tp>
+struct iterator_traits<Tp *> {
+ public:
+  // Member type
+  typedef std::ptrdiff_t difference_type;
+  typedef Tp value_type;
+  typedef Tp *pointer;
+  typedef Tp &reference;
+  typedef std::random_access_iterator_tag iterator_category;
+};
+
+// template specialization <const T *>
+template <typename Tp>
+struct iterator_traits<const Tp *> {
+ public:
+  // Member type
+  typedef std::ptrdiff_t difference_type;
+  typedef typename ft::T value_type;
+  typedef const typename T *pointer;
+  typedef const typename T &reference;
+  typedef std::random_access_iterator_tag iterator_category;
+};
+// !SECTION
+
+// SECTION 2. reverse_iterator
+template <typename Iter>
+class reverse_iterator {
+ public:
+  // SECTION 2-1. Member types
+  typedef Iter iterator_type;
+  typedef typename iterator_traits<Iter>::iterator_category iterator_category;
+  typedef typename iterator_traits<Iter>::value_type value_type;
+  typedef typename iterator_traits<Iter>::difference_type difference__type;
+  typedef typename iterator_traits<Iter>::pointer pointer;
+  typedef typename iterator_traits<Iter>::reference reference;
+  // !SECTION
+
+  // SECTION 2-2. Member functions
+  reverse_iterator();  // constructor
+  reverse_iterator &operator=(const iterator &refreverse_);
+  // !SECTION
+
+  // SECTION 2-3. Member objects
+  // TODO reverse_iterator's Member objects : current??
+  // !SECTION
+};
+
+// SECTION 2-4. Non-member functions of reverse_iterator
+/*
+// TODO reverse-iterator Non-member functions
+        operator==
+        operator!=
+        operator<
+        operator<=
+        operator>
+        operator>=
+
+        operator+
+        operator-
+*/
+// !SECTION
+// !SECTION
+
+}  // namespace ft
 #endif
--- includes/vector.hpp
+++ includes/vector.hpp
@@ -17,33 +17,83 @@
 
 namespace ft {
 
-template <class T, class Allocator = std::allocator<T>>
+// SECTION 1. vector
+template <typename T, typename Allocator = std::allocator<T>>
 class vector {
  public:
+  // SECTION 1-1. Member types
   typedef T value_type;
   typedef Allocator allocator_type;
+  typedef std::size_t size_type;
+  typedef std::ptrdiff_t difference_type;
+  typedef value_type& reference;
+  typedef const value_type& const_reference;
+  typedef typename Allocator::pointer pointer;
+  typedef typename Allocator::const_pointer const_pointer;
+  // TODO Modify iterator & conts_iterator
+  // LegacyRandomAccessIterator and LegacyContiguousIterator to value_type
+  typedef asdf iterator;
+  typedef asdf const_iterator;
+
   typedef reverse_iterator<iterator> reverse_iterator;
   typedef reverse_iterator<const_iterator> const_reverse_iterator;
+  // !SECTION
 
-  // FUNCTION : push_back, insert, swap, erase, clear,
+  // SECTION 1-2. Member Functions [element access/iterators/capacity/modifiers]
+  // Canonical class form
+  vector();
+  ~vector();
+  vector& operator=(const vector& ref);
+  // assign
+  // get_allocator
+
+  // Element access
+
+  // Iterators
+
+  // Capacity
+
+  // Modifiers
+  // !SECTION
 };
+// !SECTION
 
-template <class T, class Allocator = std::allocator<T>>
+// SECTION 2. Non-member functions
+template <typename T, typename Allocator = std::allocator<T>>
 bool operator==(const vector<T, Allocator>& x, const vector<T, Allocator>& y);
-template <class T, class Allocator = std::allocator<T>>
+template <typename T, typename Allocator = std::allocator<T>>
 bool operator<(const vector<T, Allocator>& x, const vector<T, Allocator>& y);
-template <class T, class Allocator = std::allocator<T>>
+template <typename T, typename Allocator = std::allocator<T>>
 bool operator!=(const vector<T, Allocator>& x, const vector<T, Allocator>& y);
-template <class T, class Allocator = std::allocator<T>>
+template <typename T, typename Allocator = std::allocator<T>>
 bool operator>(const vector<T, Allocator>& x, const vector<T, Allocator>& y);
-template <class T, class Allocator = std::allocator<T>>
+template <typename T, typename Allocator = std::allocator<T>>
 bool operator>=(const vector<T, Allocator>& x, const vector<T, Allocator>& y);
-template <class T, class Allocator = std::allocator<T>>
+template <typename T, typename Allocator = std::allocator<T>>
 bool operator<=(const vector<T, Allocator>& x, const vector<T, Allocator>& y);
 
-template <class T, class Allocator = std::allocator<T>>
+template <typename T, typename Allocator = std::allocator<T>>
 void swap(vector<T, Allocator>& x,
           vector<T, Allocator>& y) noexcept(noexcept(x.swap(y)));
+// !SECTION
+
+// SECTION 3. vector_iterator : random_access_iterator (normal_iterator in GNU)
+// Iterator invalidation : swap, clear, operator=, assign, earse ...
+template <typename _Iter, typename Container>
+struct vector_iterator {
+ public:
+  // Member type
+  typedef typename iterator_traits<Iter>::difference_type difference_type;
+  typedef typename iterator_traits<Iter>::value_type value_type;
+  typedef typename iterator_traits<Iter>::pointer pointer;
+  typedef typename iterator_traits<Iter>::reference reference;
+  typedef typename iterator_traits<Iter>::iterator_category iterator_category;
+
+  // iterator
+  vector_iterator() : iterator() {}
+  vector_iterator(const)
+};
+// !SECTION
 
 }  // namespace ft
 
