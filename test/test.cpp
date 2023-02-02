a10ffdbc96e281fbf874826dc9e90320326a1ddc
hannkim <hannkim@student.42seoul.kr>
2023-02-02 14:37:48 +0900

    test: test std::enable_if example

--- test/test.cpp
+++ test/test.cpp
@@ -1,49 +1,27 @@
 #include <iostream>
-using namespace std;
+#include <type_traits>
 
-template <typename T>
-class Base {
- protected:
-  T asset;
-  int test;
-
- public:
-  Base(T a) {
-    asset = a;
-    test = 42;
+template <typename _Tp>
+void catch_type(typename std::enable_if<std::is_void<_Tp>::value>::type* = 0) {
+  if (std::is_void<_Tp>::value) {
+    std::cout << "void" << std::endl;
+  } else {
+    std::cout << "not void" << std::endl;
   }
-  ~Base() { cout << "~Base Destructor called" << endl; }
-  void showBase() { cout << asset << endl; }
-};
+}
 
-template <typename T>
-class Derived : public Base<T> {
-  // int asset;
-  int test;
-
- public:
-  // Derived(int a, int b) : Base<T>::Base(a) { test = b; }
-  Derived(int a, int b) : Base<T>(a) { test = b; }
-  // Derived(int a, int b) : Base::Base(a) { test = b; }
-  ~Derived() { cout << "~Derived called" << endl; }
-
-  void showDerived() {
-    cout << "Base::asset : " << Base<T>::asset << endl;
-    cout << "asset : " << this->asset << endl;
-    cout << "test : " << test << endl;
-    cout << "Base::test : " << Base<T>::test << endl;
-  }
-
-  void test_function() {
-    Derived tmp(11, 22);
-
-    tmp.asset = 1;
-    cout << "tmp.asset : " << tmp.asset << endl;
-  }
-};
+// template <typename _Tp>
+// void catch_type(_Tp* = 0) {
+//   if (std::is_void<_Tp>::value) {
+//     std::cout << "void" << std::endl;
+//   } else {
+//     std::cout << "not void" << std::endl;
+//   }
+// }
 
 int main() {
-  Derived<int> D(10, 50);
+  catch_type<void>();
+  catch_type<int>();
 
   return 0;
 }
