9f5a0490be701eb161574b1af85b016b9dd95609
Kimhannah <skqkdldhf98@naver.com>
2023-02-03 15:00:40 +0900

    feat: Complete ft::equal, ft::lexicographical_compare function

--- includes/algorithm.hpp
+++ includes/algorithm.hpp
@@ -13,40 +13,72 @@
 
 namespace ft {
 // SECTION equal
+/**
+ * @brief equality version
+ * @note using operator==
+ */
 template <typename InputIt1, typename InputIt2>
 bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2) {
-  while (first1 != last1) {
-    if (!(*first1 ==
-          *first2))  // or: if (!pred(*first1,*first2)), for version 2
+  for (; first1 != last1; ++first1, ++first2) {
+    if (!(*first1 == *first2)) {
       return false;
-    ++first1;
-    ++first2;
+    }
   }
   return true;
 }
 
+// predicate version
+/**
+ * @brief predicate version
+ * @note using binary predicate function
+ *
+ * @param pred  bool cmp(const Type1 &a, const Type2 &b);
+ */
 template <typename InputIt1, typename InputIt2, typename BinaryPredicate>
-bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2, BinaryPredicate p);
+bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2,
+           BinaryPredicate pred) {
+  for (; first1 != last1; ++first1, ++first2) {
+    if (!pred(*first1, *first2)) {
+      return false;
+    }
+  }
+  return true;
+}
 // !SECTION
 
 // SECTION lexicographical_compare
+/**
+ * @brief default version
+ * @note using operator<
+ */
 template <typename InputIt1, typename InputIt2>
 bool lexicographical_compare(InputIt1 first1, InputIt1 last1, InputIt2 first2,
                              InputIt2 last2) {
-  while (first1 != last1) {
-    if (first2 == last2 || *first2 < *first1)
-      return false;
-    else if (*first1 < *first2)
-      return true;
-    ++first1;
-    ++first2;
+  for (; (first1 != last1) && (first2 != last2); ++first1, (void)++first2) {
+    if (*first1 < *first2) return true;
+    if (*first2 < *first1) return false;
   }
-  return (first2 != last2);
+
+  return (first1 == last1) && (first2 != last2);
 }
 
+/**
+ * @brief custom version
+ * @note using binary comparison function comp
+ *
+ * @param comp bool cmp(const Type1 &a, const Type2 &b);
+ */
 template <typename InputIt1, typename InputIt2, typename Compare>
 bool lexicographical_compare(InputIt1 first1, InputIt1 last1, InputIt2 first2,
-                             InputIt2 last2, Compare comp);
+                             InputIt2 last2, Compare comp) {
+  for (; (first1 != last1) && (first2 != last2); ++first1, (void)++first2) {
+    if (comp(*first1, *first2)) return true;
+    if (comp(*first2, *first1)) return false;
+  }
+
+  return (first1 == last1) && (first2 != last2);
+}
+
 // !SECTION
 }  // namespace ft
 
